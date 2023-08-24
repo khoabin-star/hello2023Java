1b7b318c7a7d8902a7dc43b8ad5ae36354182034
Khoa Tran <ktran18@huskers.unl.edu>
2023-08-23 23:54:50 -0500

    intial commit

--- src/unl/cse/Hello.java
+++ src/unl/cse/Hello.java
@@ -4,6 +4,7 @@ public class Hello {
 	public static void main (String args[]) {
 		System.out.println("Hello my name is Khoa");
 		System.out.println("My major is Computer Science");
+		System.out.println("It is really nice to meet you");
 		
 	}
 }
